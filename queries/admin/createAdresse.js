26a18acd3fb18642ef6bff72a92aa5226279582a
Emanu <emanu@emanusprojects.com>
2024-04-03 13:59:14 -0400

    Final client and employee pages

--- queries/admin/createAdresse.js
+++ queries/admin/createAdresse.js
@@ -17,27 +17,34 @@ export default {
       req.query.province,
       req.query.rue,
       req.query.num_rue,
-      req.query.code_postal
+      req.query.code_postal,
     ];
     try {
-        await pg.query(query, values);
-// TODO ADD TRY CATCH IN LOCAL FOR EACH SCOPE OR CONVERT FOR EACH TO FOR OF LOOP
-        let num_tels = req.query.num_tels.split(";")
-        let num_query = `INSERT INTO num_tel(numero, id_adresse) VALUES ($1, CAST($2 as INTEGER))`
-        num_tels.forEach(async num_tel => {
-            await pg.query(num_query, [String(parseInt(num_tel)), id])
-        })
+      await pg.query(query, values);
+      let num_tels = req.query.num_tels.split(";");
+      let num_query = `INSERT INTO num_tel(numero, id_adresse) VALUES ($1, CAST($2 as INTEGER))`;
+      for (const num_tel of num_tels) {
+        await pg.query(num_query, [String(parseInt(num_tel)), id]);
+      }
 
-        let emails = req.query.emails.split(";");
-        let emails_query = `INSERT INTO email(email, id_adresse) VALUES ($1, CAST($2 as INTEGER))`;
-        emails.forEach(async (email) => {
-            console.log(email)
-            await pg.query(emails_query, [email, id]);
-        });
+      let emails = req.query.emails.split(";");
+      let emails_query = `INSERT INTO email(email, id_adresse) VALUES ($1, CAST($2 as INTEGER))`;
+      for (const email of emails) {
+        console.log(email);
+        await pg.query(emails_query, [email, id]);
+      }
     } catch (e) {
-        query = `DELETE FROM adresse WHERE id_adresse = CAST($1 as INTEGER)`;
-        await pg.query(query, [id]);
-        return res.send("ERROR: " + e.message);
+      let query1 = `DELETE FROM num_tel WHERE id_adresse = CAST($1 as INTEGER);`;
+      let query2 = `DELETE FROM email WHERE id_adresse = CAST($1 as INTEGER); `;
+      let query3 = `DELETE FROM adresse WHERE id_adresse = CAST($1 as INTEGER);`;
+      await pg.query(query1, [id]);
+      await pg.query(query2, [id]);
+      await pg.query(query3, [id]);
+      return res.send(
+        "ERROR: " +
+          e.message +
+          "<br><br>Assurez-vous:<br>- Que tous les champs respectent le format prescrit<br>- Que cette adresse, numéro de téléphone ou email n'existe pas déjà dans la base de données"
+      );
     }
 
     res.send(String(id));
